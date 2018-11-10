0e1fe9961f0bc678ffd12b2de34115a89b0c9ef6
Boyan Naydenov <boyan.naydenov@gmail.com>
2018-11-10 15:50:05 +0100

    Added Menu screen to start the game and a finish screen to start over.

--- src/game/GameEngine.java
+++ src/game/GameEngine.java
@@ -6,6 +6,8 @@ import java.util.TimerTask;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -13,6 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -24,11 +27,6 @@ import javafx.stage.Stage;
 
 public abstract class GameEngine extends Application implements Runnable {
 
-	// ***************************************************************************** //
-	// *                                                                           * //
-	// *                      EDIT CONSTANTS AS YOU LIKE                           * //
-	// *                                                                           * //
-	// ***************************************************************************** //
 	/** Speed of the game */
 	static public final long DELAY_MS = 150L;
 
@@ -40,138 +38,127 @@ public abstract class GameEngine extends Application implements Runnable {
 
 	/** The size of all the elements building up the grid */
 	static public final double ELEMENT_SIZE = 24;
-	
+
 	/** The number of lines/columns in the grid */
 	static public final int GRID_SIZE = 24;
 
-	// ***************************************************************************** //
-	// *                                                                           * //
-	// *                   DO NOT EDIT THE FOLLOWING ATTRIBUTES                    * //
-	// *                                                                           * //
-	// ***************************************************************************** //
-
 	/** Graphical window size (square) */
-	static public final double WINDOW_SIZE = GRID_SIZE*ELEMENT_SIZE;
-	
+	static public final double WINDOW_SIZE = GRID_SIZE * ELEMENT_SIZE;
+
 	/** Root of the Java FX scene graph containing all the elements to display */
-	private Group root;
+	private StackPane root;
+	private Group rootGame;
+	
+	protected Scene scene;
+	private Scene game;
+	protected Stage primaryStage;
 	
 	/** The timer for scheduling next game step */
 	private Timer timer;
-
-	// ***************************************************************************** //
-	// *                                                                           * //
-	// *                         DO NOT EDIT PAST HERE                             * //
-	// *                                                                           * //
-	// ***************************************************************************** //
-
-	/** 
+	
+	/**
 	 * Initialize the graphical display.
 	 *
 	 * In a Java FX application, this is a mandatory replacement of the constructor.
 	 */
 	@Override
 	public void start(Stage primaryStage) {
-		// Create the root of the graph scene, and all its children 
-		root = new Group();
+		this.primaryStage = primaryStage;
+		// Create the root of the graph scene, and all its children
+		root = new StackPane();
+		rootGame = new Group();
 
 		// create the scene graph
-		Scene scene = new Scene(root, WINDOW_SIZE, WINDOW_SIZE, BG_COLOR);
+		scene = new Scene(root, WINDOW_SIZE, WINDOW_SIZE, BG_COLOR);
+		game = new Scene(rootGame, WINDOW_SIZE, WINDOW_SIZE, BG_COLOR);
 		
 		// add a KeyEvent listener to the scene
-		scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> Keyboard.storeLastKeyCode(event.getCode()));
-		
-		// terminate the Application when the window is closed
-		primaryStage.setOnCloseRequest(event -> { Platform.exit(); System.exit(0); } );
+		game.addEventHandler(KeyEvent.KEY_PRESSED, event -> Keyboard.storeLastKeyCode(event.getCode()));
 
-		// Create the timer
-		timer = new Timer();
-		
+		// terminate the Application when the window is closed
+		primaryStage.setOnCloseRequest(event -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		welcomeScreen(primaryStage,scene,root);
 		// Show a graphical window with all the graph scene content
 		primaryStage.setScene(scene);
 		primaryStage.setTitle(TITLE);
 		primaryStage.setResizable(false);
-		primaryStage.show();	
+		primaryStage.show();
 
-		// Schedule the first game step.
-		// Usually a JavaFX game would use a TimeLine object in order to define the KeyFrames
-		// of an animation.
-		// However here we anticipate the fact that a Game would have to be synchronized on
-		// two separate computers : they have their own clocks, and there's always a drift
-		// (or skew) between two clocks.
-		// So, we decide to use the good old Timer instead, and don't use a strictly periodic
-		// TimerTask : the next delay will only be scheduled at the end of a game step,
-		// allowing the timers on two different computers to roughly synchronize when 
-		// exchanging messages.
-		timer.schedule(new GameTimerTask(this), DELAY_MS);
-	}	
-	
+	}
 
 	private class GameTimerTask extends TimerTask {
 		private Runnable gameStep;
-		
-		public GameTimerTask(Runnable gameStep) { this.gameStep = gameStep; }
+
+		public GameTimerTask(Runnable gameStep) {
+			this.gameStep = gameStep;
+		}
 
 		// The JavaFX thread is the only one that should draw on screen.
 		// This run method will be executed by a timer thread,
 		// so we gently ask the JavaFX to run our gameStep when he has
 		// finished its other duties (hopefully as soon as possible)
-		public void run() {	Platform.runLater(gameStep); }
+		public void run() {
+			Platform.runLater(gameStep);
+		}
 
 	}
 
-	// This is the run method that will be executed roughly periodically by the JavaFX thread.
+	// This is the run method that will be executed roughly periodically by the
+	// JavaFX thread.
 	// It could be much more optimized wrt to the graph scene updating,
 	// but this should be enough for simple games.
 	final public void run() {
 		try {
-			root.getChildren().setAll(gameStep());
+			rootGame.getChildren().setAll(gameStep());
 			timer.schedule(new GameTimerTask(this), DELAY_MS);
 		} catch (Exception e) {
-			  //Creating a Text object 
-			  Rectangle backgroundFill = new Rectangle(WINDOW_SIZE, WINDOW_SIZE, Color.ALICEBLUE);
-			  
-			  Text text = new Text(); 
-			  text.setText("Your snake has crashed!");
-			  //Setting font to the text 
-			  text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
-			  text.setFill(Color.RED);
-			  //setting the position of the text
-			  text.setX(WINDOW_SIZE/4); 
-			  text.setY(WINDOW_SIZE/2); 
-			  
-			  Button buttonExit = new Button("Exit");
-//				buttonExit.setLayoutX(WINDOW_SIZE/2);
-//				buttonExit.setLayoutY(WINDOW_SIZE/1.3);
-			  Button buttonStartAgain = new Button("Start Again");
-//				buttonStartAgain.setLayoutX(WINDOW_SIZE/2);
-//				buttonStartAgain.setLayoutY(WINDOW_SIZE/1.2);
-			  
-			  VBox vbox = new VBox();
-
-		      vbox.setSpacing(10);
-		      vbox.setPadding(new Insets(5));
-		      vbox.setAlignment(Pos.CENTER_LEFT);
-
-		      vbox.getChildren().addAll(buttonExit,buttonStartAgain);
-		      
-			  
-			  root.getChildren().add(backgroundFill);
-			  root.getChildren().add(text);
-			  root.getChildren().add(vbox);
-//			  root.getChildren().add(buttonExit);
-//			  root.getChildren().add(buttonStartAgain);
-			  System.out.println(e);
-		}		
+			finalScreen(primaryStage,scene,root);
+			System.out.println(e);
+		}
 	}
+
+
 	
+	public void gameScreen() {
+		rootGame.getChildren().clear();
+		primaryStage.setScene(game);
+		primaryStage.setTitle(TITLE);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		// Create the timer
+		timer = new Timer();
+		// Schedule the first game step.
+		// Usually a JavaFX game would use a TimeLine object in order to define the
+		// KeyFrames
+		// of an animation.
+		// However here we anticipate the fact that a Game would have to be synchronized
+		// on
+		// two separate computers : they have their own clocks, and there's always a
+		// drift
+		// (or skew) between two clocks.
+		// So, we decide to use the good old Timer instead, and don't use a strictly
+		// periodic
+		// TimerTask : the next delay will only be scheduled at the end of a game step,
+		// allowing the timers on two different computers to roughly synchronize when
+		// exchanging messages.
+		timer.schedule(new GameTimerTask(this), DELAY_MS);
+		
+	}
+
+
+
 	/**
 	 * Implement your own gameStep method for your game.
 	 * 
-	 * @return a Collection of Node that will form the new JavaFX graph scene.
-	 * All theses nodes will be displayed on screen, except if their coordinates are
-	 * outside the window bounds...
-	 * @throws Exception 
+	 * @return a Collection of Node that will form the new JavaFX graph scene. All
+	 *         theses nodes will be displayed on screen, except if their coordinates
+	 *         are outside the window bounds...
+	 * @throws Exception
 	 */
 	public abstract Collection<Node> gameStep() throws Exception;
+	public abstract void welcomeScreen(Stage primaryStage, Scene scene, StackPane root);
+	public abstract void finalScreen(Stage primaryStage, Scene scene, StackPane root);
 }
--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -6,25 +6,34 @@ import java.util.Random;
 import java.util.Set;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GameMain extends GameEngine {
 	private Snake snake1;
 	private Snake snake2;
 	private ArrayList<Node> allNodes;
-	private Set<Position> allPositions;
 	
 	public GameMain() 
 	{
-		allPositions = new HashSet<Position>();
-		
-		Position startPos1 = new Position((new Random().nextInt((int)GRID_SIZE-2)+1)*24, WINDOW_SIZE/2);
-		Position startPos2 = new Position((new Random().nextInt((int)GRID_SIZE-2)+1)*24, WINDOW_SIZE/2);
-		snake1 = new Snake(startPos1, Color.AQUAMARINE, "R");
-		snake2 = new Snake(startPos2, Color.GREEN, "L");
-		
-		this.allNodes = new ArrayList<Node>();
+		initializeGame();
 	}
 	
 	/** Application main method	 */
@@ -49,5 +58,102 @@ public class GameMain extends GameEngine {
 		return allNodes;
 	}
 	
+	public void welcomeScreen(Stage primaryStage, Scene scene, StackPane root) {
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(TITLE);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		
+		Text welcomeText = new Text();
+		welcomeText.setText("B L O C K A D E");
+		// Setting font to the text
+		welcomeText.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+		welcomeText.setFill(Color.RED);
+		
+		Button btnStart = new Button("Start Game");
+		
+		btnStart.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                gameScreen();
+            }
+        });
+		
+		Text author = new Text();
+		author.setText("Boyan Naydenov");
+		// Setting font to the text
+		author.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 10));
+		author.setFill(Color.BLACK);
+		
+		VBox vbox = new VBox();
+
+		vbox.setSpacing(10);
+		vbox.setPadding(new Insets(5));
+		vbox.setAlignment(Pos.CENTER);
+
+		vbox.getChildren().addAll(welcomeText, btnStart, author);
+		     
+	     
+		root.getChildren().setAll(vbox);
+		StackPane.setAlignment(root, Pos.CENTER);
+	}
+	public void initializeGame() {
+		Position startPos1 = new Position((new Random().nextInt((int)GRID_SIZE-2)+1)*24, WINDOW_SIZE/2);
+		Position startPos2 = new Position((new Random().nextInt((int)GRID_SIZE-2)+1)*24, WINDOW_SIZE/2);
+		snake1 = new Snake(startPos1, Color.AQUAMARINE, "R");
+		snake2 = new Snake(startPos2, Color.GREEN, "L");
+		
+		this.allNodes = new ArrayList<Node>();
+	}
+	public void finalScreen(Stage primaryStage, Scene scene, StackPane root) {
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(TITLE);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		// Creating a Text object
+		Rectangle backgroundFill = new Rectangle(WINDOW_SIZE, WINDOW_SIZE, Color.ALICEBLUE);
+
+		Text text = new Text();
+		text.setText("Your snake has crashed!");
+		// Setting font to the text
+		text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+		text.setFill(Color.RED);
+
+		Button buttonExit = new Button("Exit");
+		buttonExit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+        		Platform.exit();
+        		System.exit(0);
+            }
+        });
+		Button buttonStartAgain = new Button("Start Again");
+		
+		buttonStartAgain.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	initializeGame();
+                welcomeScreen(primaryStage, scene, root);
+            }
+        });
+
+		Text author = new Text();
+		author.setText("Boyan Naydenov");
+		// Setting font to the text
+		author.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 10));
+		author.setFill(Color.BLACK);
+		
+		VBox vbox = new VBox();
+
+		vbox.setSpacing(10);
+		vbox.setPadding(new Insets(5));
+		vbox.setAlignment(Pos.CENTER);
+
+		vbox.getChildren().addAll(text,buttonExit, buttonStartAgain, author);
+
+		root.getChildren().add(backgroundFill);
+		root.getChildren().add(vbox);
+		StackPane.setAlignment(root, Pos.CENTER);
+	}
 
 }
